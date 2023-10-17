a72065da2df439aa6a855dbe4d367e353b779fde
jayshree <jayshreechauhan2904@gmail.com>
2023-10-17 12:15:17 +0530

    save all

---
+++ basicreact/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(0);
+
+  function updateData() {
+    setData(data + 1)
+  }
+
+  return (
+    <>
+      <h1>{data}</h1>
+      <button onClick={updateData}>Update Data</button>
+    </>
+  );
+}
+
+export default App;
