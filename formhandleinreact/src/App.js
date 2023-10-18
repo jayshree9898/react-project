36a1689bff64b3cf67a77a80a852d72771ca48cd
jayshree <jayshreechauhan2904@gmail.com>
2023-10-18 14:19:14 +0530

    save conditionalrendring and formhandle

--- formhandleinreact/src/App.js
+++ formhandleinreact/src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [tnc, setTnc] = useState(false);
+  const [interest, setInterest] = useState("");
+
+  function getFormData(e) {
+    console.log(name, tnc, interest);
+    e.preventDefault();
+  }
+
+  function clearForm() {
+    setName("");
+    setTnc(false);
+    setInterest("");
+  }
+
   return (
     <div className="App">
 
+      <h1>Form handle in React</h1>
+
+      <form onSubmit={getFormData}>
+
+        <input type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} /><br /><br />
+
+        <select value={interest} onChange={(e) => setInterest(e.target.value)}>
+          <option value="">Select Option</option>
+          <option value="Marvel">Marvel</option>
+          <option value="Dc">Dc</option>
+        </select><br /><br />
+
+        <input type="checkbox" checked={tnc} onChange={(e) => setTnc(e.target.checked)} /><span>Accept Terms And Conditions</span>
+        <br /><br />
+
+        <button type="submit">Submit</button>
+        <button type="button" onClick={clearForm}>Clear</button>
+      </form>
     </div>
   );
 }
