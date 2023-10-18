c1694809dd9679efc96b78301c0345b3ee57bbc7
jayshree <jayshreechauhan2904@gmail.com>
2023-10-18 15:13:57 +0530

    update login

--- basicformvalidation/src/login.js
+++ basicformvalidation/src/login.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+
 function Login() {
+
+    const [userId, setUserId] = useState("")
+    const [userPass, setUserPass] = useState("")
+
+    function loginHandle(e) {
+        e.preventDefault()
+    }
+
+    function userHandler(e) {
+        console.log(e.target.value);
+    }
+
     return (
         <>
-            <h1> Login</h1>
-            <input type="text" placeholder="Enter User ID" />
-            <br /><br />
-            <input type="text" placeholder="Enter User Password" />
-            <br /><br />
-            <button>Login</button>
-
+            <form onSubmit={loginHandle}>
+                <h1> Login</h1>
+                <input type="text" placeholder="Enter User ID" onChange={userHandler} />
+                <br /><br />
+                <input type="text" placeholder="Enter User Password" />
+                <br /><br />
+                <button type="submit">Login</button>
+            </form>
         </>
     )
 }
