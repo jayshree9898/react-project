3222b245ea80e3eb24af814a9d5a19c87dde3a57
jayshree <jayshreechauhan2904@gmail.com>
2023-10-18 14:51:47 +0530

    Some Changes

---
+++ basicformvalidation/src/login.js
@@ -0,0 +1,15 @@
+function Login() {
+    return (
+        <>
+            <h1> Login</h1>
+            <input type="text" placeholder="Enter User ID" />
+            <br /><br />
+            <input type="text" placeholder="Enter User Password" />
+            <br /><br />
+            <button>Login</button>
+
+        </>
+    )
+}
+
+export default Login
