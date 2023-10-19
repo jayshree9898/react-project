820ea64f4a57e8fe3cb2bd4e5964c0d488b00552
jayshree <jayshreechauhan2904@gmail.com>
2023-10-19 09:10:03 +0530

    form validation some changes

--- basicformvalidation/src/login.js
+++ basicformvalidation/src/login.js
@@ -2,24 +2,55 @@ import { useState } from 'react';
 
 function Login() {
 
-    const [userId, setUserId] = useState("")
-    const [userPass, setUserPass] = useState("")
+    const [userId, setUserId] = useState("");
+    const [userPass, setUserPass] = useState("");
+    const [userErr, setUserErr] = useState(false);
+    const [passErr, setPassErr] = useState(false);
 
     function loginHandle(e) {
+
+        if (userId.length < 3 || userPass.length < 3) {
+            alert("type Correct Values")
+        }
+        else {
+            alert("All good")
+        }
         e.preventDefault()
     }
 
     function userHandler(e) {
-        console.log(e.target.value);
+        let item = e.target.value
+
+        if (item.length < 3) {
+            setUserErr(true)
+        }
+        else {
+            setUserErr(false)
+        }
+        setUserId(item)
+
+    }
+
+    function passHandler(e) {
+        let item = e.target.value
+
+        if (item.length < 3) {
+            setPassErr(true)
+        }
+        else {
+            setPassErr(false)
+        }
+        setUserPass(item)
+
     }
 
     return (
         <>
             <form onSubmit={loginHandle}>
                 <h1> Login</h1>
-                <input type="text" placeholder="Enter User ID" onChange={userHandler} />
+                <input type="text" placeholder="Enter User ID" onChange={userHandler} />{userErr ? <span>User Not Valid</span> : ""}
                 <br /><br />
-                <input type="text" placeholder="Enter User Password" />
+                <input type="password" placeholder="Enter User Password" onChange={passHandler} />{passErr ? <span>Invalid Password</span> : ""}
                 <br /><br />
                 <button type="submit">Login</button>
             </form>
